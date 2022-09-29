3735601ea8ad5d14941608897c874fa1594bb374
sgteryn <sgteryn@gmail.com>
2022-09-29 00:52:34 -0400

    commit section 2 part 2

--- basePage.ts
+++ basePage.ts
@@ -1,3 +1,4 @@
+//Below are dependencies imported from the selenium-webdriver to be available to use in this page for relevant and necessary code.
 import {Builder, By, Capabilities, until, WebDriver, WebElement} from 'selenium-webdriver'
 const chromedriver = require('chromedriver')
 
@@ -16,22 +17,23 @@ export class BasePage {
         this.driver = new Builder().withCapabilities(Capabilities.chrome()).build()
         if(options && options.url) this.url = options.url
     }
+    //This method below allows the test automation to navigate to the url set forth, it also makes provisions in the case that a url is not provided with a promise.reject
     async navigate(url?: string): Promise<void> {
         if (url) return await this.driver.get(url)
         else if (this.url) return await this.driver.get(this.url)
         else return Promise.reject('You need a url to visit the page please add one in the page objects or in your test')
     }
-
+     //This method below allows the test automation to go to a specified element and show that it is present, returning that specified element
     async getElement(elementBy: By): Promise<WebElement> {
         await this.driver.wait(until.elementLocated(elementBy))
         let element = await this.driver.findElement(elementBy)
         await this.driver.wait(until.elementIsVisible(element))
         return element
     }
-
+    // The method below is allowing the test automation to click a specified element.
     async click(elementBy: By): Promise<void> {
         return(await this.getElement(elementBy)).click()
-    }
+    }  
 
     async setInput(elementBy:By, keys: any): Promise<void> {
         let input = await this.getElement(elementBy)
