fb5fbcaae6e770b233f38f45e066b0db83aa3d04
sgteryn <sgteryn@gmail.com>
2022-09-30 15:10:32 -0400

    update tests

--- miniCapstone.test.ts
+++ miniCapstone.test.ts
@@ -2,13 +2,46 @@ import {By, Builder, Capabilities, WebDriver, until} from 'selenium-webdriver'
 import {Clicker} from './miniPageObjects'
 
 const clicker = new Clicker()
-const driver: WebDriver = new Builder().withCapabilities(Capabilities.chrome()).build()
 
 
-test('Legacy button functionality', async () => {
+test('Click the cookie to make cookies', async () => {
+
     await clicker.navigate('https://orteil.dashnet.org/cookieclicker/')
 
     await clicker.click(By.xpath("//div[@id='langSelect-EN']")) 
+
+    await clicker.driver.wait(until.elementLocated(By.id("bigCookie"))).click()
+
+    await clicker.click(By.xpath("//*[@id='statsButton']"))
+
+    await clicker.getElement((By.xpath("//*[@id='statsGeneral']//child::div[1]")))
+    clicker.driver.wait(until.stalenessOf(clicker.driver.wait(until.elementLocated((By.xpath("//*[@id='statsGeneral']//child::div[1]"))))));
+
+    
+    let cookiesInBank = await clicker.driver.findElement(By.xpath("//*[@id='statsGeneral']//child::div[1]")).getText()
+    
+    expect(cookiesInBank).toContain('1')
+
+})
+
+test('Purchase cursor', async ()=> {
+    // await clicker.navigate('https://orteil.dashnet.org/cookieclicker/')
+    
+    // await clicker.repeatClick(15, (driver.findElement(By.xpath("//button[@id='bigCookie']"))))
+
+    await clicker.driver.findElement(By.xpath("//div[text()='Cursor']")).click()
+
+    let clickersPurchased = await clicker.driver.findElement(By.xpath("//div[@id='productOwned0']")).getText()
+
+    expect(clickersPurchased).toBe(1)
+
+})
+
+
+test('Legacy button functionality', async () => {
+    // await clicker.navigate('https://orteil.dashnet.org/cookieclicker/')
+
+    // await clicker.click(By.xpath("//div[@id='langSelect-EN']")) 
 
     await clicker.click((By.xpath("//*[@id='legacyButton']"))); 
     clicker.driver.wait(until.stalenessOf(clicker.driver.wait(until.elementLocated(By.xpath("//*[@id='legacyButton']")))));
@@ -32,32 +65,6 @@ test('Ascend button present', async () => {
     let AscendButtonPresent = clicker.driver.findElement(By.xpath("//*[@id='ascendBG']"))
 
     expect(AscendButtonPresent).toBeTruthy()
-})
-
-
-test('Click the cookie to make cookies', async () => {
-
-    await clicker.navigate('https://orteil.dashnet.org/cookieclicker/')
-
-    await clicker.driver.findElement(By.xpath("//button[@id='bigCookie']")).click()
-    
-    let cookiesMade = await driver.findElement(By.xpath('//div[@id="cookieNumbers"]')).getText()
-    
-    expect(cookiesMade).toBe(1)
-
-})
-
-test('Purchase cursor', async ()=> {
-    await clicker.navigate('https://orteil.dashnet.org/cookieclicker/')
-    
-    // await clicker.repeatClick(15, (driver.findElement(By.xpath("//button[@id='bigCookie']"))))
-
-    await clicker.driver.findElement(By.xpath("//div[text()='Cursor']")).click()
-
-    let clickersPurchased = await clicker.driver.findElement(By.xpath("//div[@id='productOwned0']")).getText()
-
-    expect(clickersPurchased).toBe(1)
-
 })
 
 afterAll(async () => {
